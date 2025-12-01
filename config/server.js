b8d1d246b249f6ae6d0c82bdbac815b309893071
Sergio-GM <111613100+Sergio-GM@users.noreply.github.com>
2025-12-01 17:15:25 -0600

    Update server.js

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    // Es FUNDAMENTAL que esto lea la variable de entorno y la convierta a un array.
+    // ESTO ES LO CRÍTICO: Debe leer APP_KEYS y convertirlo en un array.
     keys: env.array('APP_KEYS'), 
   },
 });
