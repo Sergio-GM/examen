46b28bc4306e48f5c8a97b3d941a70a2b8c5600b
Sergio-GM <111613100+Sergio-GM@users.noreply.github.com>
2025-12-01 17:08:35 -0600

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,10 +1,9 @@
+// config/server.js
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  // Esta sección es CRÍTICA
   app: {
-    // Strapi espera un array de claves. 
-    // env.array() divide la cadena de la variable de entorno por comas.
+    // Es FUNDAMENTAL que esto lea la variable de entorno y la convierta a un array.
     keys: env.array('APP_KEYS'), 
   },
 });
