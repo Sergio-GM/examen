78bd4a80d70ee4c1bc7353d46b86f821552d887e
Sergio-GM <111613100+Sergio-GM@users.noreply.github.com>
2025-12-01 20:59:24 -0600

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,9 +1,15 @@
-// config/server.js
+// /config/server.js
+
 module.exports = ({ env }) => ({
+  // Estas líneas resuelven los errores de HOST y PORT
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  
   app: {
-    // Usa 'keys' y 'env.array'
-    keys: env.array('APP_KEYS'), 
+    // La función env.array() convierte la cadena de la variable de entorno 
+    // 'APP_KEYS' en un array de strings, que es lo que Strapi requiere.
+    keys: env.array('APP_KEYS'),
   },
+
+  // Puedes añadir otras configuraciones aquí si es necesario
 });
