80173ae52a991967df52c7feca080a63594baf6b
Sergio-GM <111613100+Sergio-GM@users.noreply.github.com>
2025-12-01 20:43:22 -0600

    Delete config/config directory

--- config/config/server.js
+++
@@ -1,9 +0,0 @@
-// config/server.js
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    // ESTO ES LO CRÍTICO: Debe leer APP_KEYS y convertirlo en un array.
-    keys: env.array('APP_KEYS'), 
-  },
-});
