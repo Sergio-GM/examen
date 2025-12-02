2916c1b6c572e384f0f7dd90e590d2400b538389
Sergio-GM <111613100+Sergio-GM@users.noreply.github.com>
2025-12-01 21:11:31 -0600

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,13 +1,8 @@
-// /config/server.js
+// config/server.js
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  
-  // ESTA ES LA CONFIGURACIÓN CRÍTICA PARA APP_KEYS
   app: {
-    // env.array() lee la cadena de 'APP_KEYS' y la convierte en un array de claves.
-    keys: env.array('APP_KEYS'), 
+    keys: env.array('APP_KEYS'), // Esto resuelve el error de las claves.
   },
-  
-  // Puedes dejar el resto de tu configuración aquí...
 });
